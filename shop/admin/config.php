77d5aa5d35f0550c29398aaed1fa1a4a150672ad
Lynn Stahl <msbtterswrth@gmail.com>
2016-02-12 15:09:33 -0500

    init

---
+++ shop/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.charmcitysweets.com/admin/');
+define('HTTP_CATALOG', 'http://shop.charmcitysweets.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.charmcitysweets.com/admin/');
+define('HTTPS_CATALOG', 'http://shop.charmcitysweets.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/admin/');
+define('DIR_SYSTEM', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/');
+define('DIR_LANGUAGE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/admin/language/');
+define('DIR_TEMPLATE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/admin/view/template/');
+define('DIR_CONFIG', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/config/');
+define('DIR_IMAGE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/image/');
+define('DIR_CACHE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/download/');
+define('DIR_LOGS', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/modification/');
+define('DIR_UPLOAD', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/upload/');
+define('DIR_CATALOG', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kvpbsdqv_ocar765');
+define('DB_PASSWORD', '-y[VP31S0R');
+define('DB_DATABASE', 'kvpbsdqv_ocar765');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocgn_');
---
+++ shop/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.charmcitysweets.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.charmcitysweets.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/catalog/');
+define('DIR_SYSTEM', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/');
+define('DIR_LANGUAGE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/catalog/language/');
+define('DIR_TEMPLATE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/catalog/view/theme/');
+define('DIR_CONFIG', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/config/');
+define('DIR_IMAGE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/image/');
+define('DIR_CACHE', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/download/');
+define('DIR_LOGS', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/modification/');
+define('DIR_UPLOAD', '/home/kvpbsdqv/public_html/charmcitysweets.com/shop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kvpbsdqv_ocar765');
+define('DB_PASSWORD', '-y[VP31S0R');
+define('DB_DATABASE', 'kvpbsdqv_ocar765');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocgn_');
